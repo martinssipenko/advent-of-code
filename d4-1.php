5257d692e9e3a4fe2aba1031fc1060fc470ee98f
Martins Sipenko <martins.sipenko@gmail.com>
2015-12-04 16:57:43 +0200

    Fix return

--- d4-1.php
+++ d4-1.php
@@ -1,13 +1,10 @@
 <?php
 
-$input = 'bgvyzdsv';
-
 function d4_1($input) {
     $i = 0;
     while (true) {
         if (substr(md5($input.$i), 0, 5) === '00000') {
-            var_dump($i);
-            die();
+            return $i;
         }
         $i++;
     }
